9add18ec2390468b8f76337e2e1f9fc8ae36e1a3
Simon <sqeebs@gmail.com>
2018-08-20 14:18:53 +0200

    Global vs. local variables

--- basics/scope.js
+++ basics/scope.js
@@ -4,6 +4,7 @@
 // In a scope you can access all variables in that scope and his parent/ancestor scope
 let varOne = 'varOne'
 
+let varFive = 'varFiveGlobal'
 if (true) {
     console.log(varOne)
     let varTwo = 'varTwo'
@@ -12,9 +13,12 @@ if (true) {
         let varFour = 'varFour'
         console.log(varFour)
     }
+    let varFive = 'varFiveLocal'
+    console.log(varFive)
 }
 if (true) {
     let varThree = 'varThree'
     console.log(varThree)
 }
+console.log(varFive)
 // our of scope:  console.log(varTwo)
