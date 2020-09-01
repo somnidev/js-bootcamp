21c90e8e4ec93651656900644024c98c9a464b26
Simon Oser <simon.oser@gmail.com>
2020-09-01 11:57:26 +0200

    Adds babel-so

---
+++ 14-cutting-edge-javascript/14-03-babel-so/boilerplate/src/input.js
@@ -0,0 +1,11 @@
+const name = 'Jen'
+console.log(name)
+
+class Hangman {
+    myMethod() {
+        return 'Testing hangman'
+    }
+}
+
+const hangman = new Hangman()
+console.log(hangman.myMethod())
